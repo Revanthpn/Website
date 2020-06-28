3790d2de9f84ff29b1f468039bdb07ec891fc3b6
Revanthpn <56716382+Revanthpn@users.noreply.github.com>
2020-06-29 01:07:22 +0530

    Responsiveness

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const hamburger = document.querySelector(".hamburger")
+let showMenu = false
 
-hamburger.addEventListener("click", () =>{
-    console.log("Test")
+const hamburger = document.querySelector(".hamburger")
+const lsection = document.querySelector(".l_section")
+const navName = document.querySelector(".nav-name")
+const links = document.querySelectorAll(".link")
+
+function toggleMenu() {
+    if (!showMenu) {
+        hamburger.classList.add("close")
+        lsection.classList.add("show")
+        navName.classList.add("hide")
+        showMenu = true
+    }
+    else {
+        hamburger.classList.remove("close")
+        lsection.classList.remove("show")
+        navName.classList.remove("hide")
+        showMenu = false
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu)
+links.forEach(item => {
+    item.addEventListener("click", toggleMenu)
 })
